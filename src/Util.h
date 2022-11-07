c933943073007e00461ed31b2f96f6de6879c426
Hussein Elfakharany <fakharany.hussein@gmail.com>
2022-11-07 10:44:03 +0100

    Init repo

---
+++ src/Util.h
@@ -0,0 +1,15 @@
+/* Headers for PBM+ */
+/* E.B.             */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned char bit;
+typedef unsigned char gray;
+typedef signed short gradient;
+
+char pm_getc(FILE *file);
+bit pm_getbit(FILE *file);
+unsigned char pm_getrawbyte(FILE *file);
+int pm_getint(FILE *file);
+
+void pm_erreur(char *);
