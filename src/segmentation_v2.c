445c88a82a141f6583fa2860cab6e2e3375a9e4d
Hussein Elfakharany <fakharany.hussein@gmail.com>
2022-11-19 21:22:06 +0100

    Added spatial segmentation

--- src/segmentation_v2.c
+++ src/segmentation_v2.c
@@ -45,10 +45,15 @@ void init_guard(int argc){
     }
 }
 
-void init_centers(gray* centers, int k, int maxval){
+void init_centers(int* centers, int k, int cols, int rows, int maxval){
 
-    for(int i=0; i<k*3; i++){
-        centers[i] = rand()%maxval;
+    for(int i=0; i<k; i++){
+        //Check if center coordinates are not repeated
+        centers[i*5] = rand()%rows;
+        centers[i*5+1] = rand()%cols;
+        centers[i*5+2] = rand()%maxval;
+        centers[i*5+3] = rand()%maxval;
+        centers[i*5+4] = rand()%maxval;
     }
 
 }
@@ -62,15 +67,21 @@ FILE* open_image(char* file){
     return image;
 }
 
-double distance(gray r2, gray r1, gray g2, gray g1, gray b2, gray b1){
+double intensity_distance(gray r2, gray r1, gray g2, gray g1, gray b2, gray b1){
     return sqrt(pow((r2-r1),2)+ pow((g2-g1),2)+pow((b2-b1),2));
 }
 
-void compute_association(gray* rgb_map, gray* centers, int* cluster_assoc, int rows, int cols, int k){
+double spatial_distance(int a2, int a1, int b2, int b1){
+    return sqrt(pow((a2-a1),2)+ pow((b2-b1),2));
+}
+
+void compute_association(gray* rgb_map, int* centers, int* cluster_assoc, int rows, int cols, int k){
 
     gray r,g,b;
+    int center_i, center_j; 
+    gray center_r, center_g, center_b;
     int i, j, m, min_index;
-    double curr_distance, min;
+    double curr_instensity_distance, curr_spatial_distance, min;
 
     for(i=0; i < rows; i++){
       for(j=0; j < cols; j++){
@@ -80,10 +91,16 @@ void compute_association(gray* rgb_map, gray* centers, int* cluster_assoc, int r
         min = -1;
         min_index = -1;
         for(m = 0; m < k; m++){
-            curr_distance = distance(centers[m*3],r,centers[m*3+1],g,centers[m*3+2],b);
-            if (curr_distance < min || min == -1){
+            center_i = centers[m*5];
+            center_j = centers[m*5+1];
+            center_r = centers[m*5+2];
+            center_g = centers[m*5+3];
+            center_b = centers[m*5+4];
+            curr_instensity_distance = intensity_distance(center_r,r,center_g,g,center_b,b);
+            curr_spatial_distance = spatial_distance(center_i,i,center_j,j);
+            if (curr_instensity_distance+curr_spatial_distance < min || min == -1){
                 min_index = m;
-                min = curr_distance;
+                min = curr_instensity_distance+curr_spatial_distance;
             }
         }
         cluster_assoc[i * cols + j] = min_index;
@@ -91,13 +108,15 @@ void compute_association(gray* rgb_map, gray* centers, int* cluster_assoc, int r
     }     
 }
 
-int compute_means(gray* centers, int* cluster_assoc, gray* rgb_map, int k, int cols, int rows){
+int compute_means(int* centers, int* cluster_assoc, gray* rgb_map, int k, int cols, int rows){
 
-    int m, i, j, rmoy, gmoy, bmoy, nb, break_iteration;
+    int m, i, j, imoy, jmoy, rmoy, gmoy, bmoy, nb, break_iteration;
 
     break_iteration = 1;
 
     for(m=0; m<k; m++){
+        imoy = 0;
+        jmoy = 0;
         rmoy = 0;
         gmoy = 0;
         bmoy = 0;
@@ -106,6 +125,8 @@ int compute_means(gray* centers, int* cluster_assoc, gray* rgb_map, int k, int c
             for(j=0; j<cols; j++){
                 if (cluster_assoc[i * cols + j] == m){
                     nb++;
+                    imoy += i;
+                    jmoy += j;
                     rmoy += rgb_map[(i * cols + j)*3];
                     gmoy += rgb_map[(i * cols + j)*3+1];
                     bmoy += rgb_map[(i * cols + j)*3+2];
@@ -114,16 +135,24 @@ int compute_means(gray* centers, int* cluster_assoc, gray* rgb_map, int k, int c
         }
         // The cluster is ignored if it does not contain any pixels
         if(nb != 0){
-            if (centers[m*3] != rmoy/nb){
-                centers[m*3] = rmoy/nb;
+            if (centers[m*5] != imoy/nb){
+                centers[m*5] = imoy/nb;
                 break_iteration = 0;
             }
-            if (centers[m*3+1] != gmoy/nb){
-                centers[m*3+1] = gmoy/nb;
+            if (centers[m*5+1] != jmoy/nb){
+                centers[m*5+1] = jmoy/nb;
                 break_iteration = 0;
             }
-            if (centers[m*3+2] != bmoy/nb){
-                centers[m*3+2] = bmoy/nb;
+            if (centers[m*5+2] != rmoy/nb){
+                centers[m*5+2] = rmoy/nb;
+                break_iteration = 0;
+            }
+            if (centers[m*5+3] != gmoy/nb){
+                centers[m*5+3] = gmoy/nb;
+                break_iteration = 0;
+            }
+            if (centers[m*5+4] != bmoy/nb){
+                centers[m*5+4] = bmoy/nb;
                 break_iteration = 0;
             }
         }
@@ -131,7 +160,7 @@ int compute_means(gray* centers, int* cluster_assoc, gray* rgb_map, int k, int c
     return break_iteration;
 }
 
-void writeInFile(FILE *fp, gray* centers, int* cluster_assoc, int nbCols, int nbRows, int maxval) {
+void writeInFile(FILE *fp, int* centers, int* cluster_assoc, int nbCols, int nbRows, int maxval) {
 
     fprintf(fp, "P6\n");
     fprintf(fp, "%d %d \n", nbCols, nbRows);
@@ -142,9 +171,9 @@ void writeInFile(FILE *fp, gray* centers, int* cluster_assoc, int nbCols, int nb
     for(int i=0; i < nbRows; i++)
       for(int j=0; j < nbCols ; j++){
         cluster_index = cluster_assoc[i * nbCols + j];
-        fprintf(fp,"%c", centers[cluster_index*3]);
-        fprintf(fp,"%c", centers[(cluster_index)*3+1]);
-        fprintf(fp,"%c", centers[(cluster_index)*3+2]);
+        fprintf(fp,"%c", centers[cluster_index*5+2]);
+        fprintf(fp,"%c", centers[(cluster_index)*5+3]);
+        fprintf(fp,"%c", centers[(cluster_index)*5+4]);
       }
 
     /* Closing */
@@ -185,8 +214,8 @@ int main(int argc, char* argv[]){
     k = atoi(argv[2]);
     max_iterations = atoi(argv[3]);
 
-    gray* centers = (gray*)malloc(3*k);
-    init_centers(centers, k, maxval);
+    int* centers = (int*)malloc(sizeof(int)*5*k); //each center will contain: col, row, r, g, b
+    init_centers(centers, k, cols, rows, maxval);
 
     int* cluster_assoc = (int*)malloc(sizeof(int)*cols*rows);
 
